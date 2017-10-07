ceb9465a5cdc6464c2c9962c61e56f2275b4f922
kyechou <forendef2846@gmail.com>
2017-10-08 00:29:26 +0800

    diskIO monitoring implemented

--- monitor.c
+++ monitor.c
@@ -7,22 +7,23 @@
 #undef _GNU_SOURCE
 #include "monitor.h"
 
-#define INTERVAL 2000	/* update every 2 sec */
-#define CPU_INT  1000000	/* wait 1 sec to read cpu usage */
+#define INTERVAL   1500		/* update every 1.5 sec */
+#define CPU_INT    1000000	/* wait 1 sec to read cpu usage */
+#define DISKIO_INT 1000000	/* wait 1 sec to read disk usage */
+#define DISKSTATS_BUF 1024	/* buffer size when reading /proc/diskstats */
 
 int	NCPU;
 
-long	memtotal;	/* MemTotal */
-long	memfree;	/* MemFree */
-long	membuffers;	/* Buffers */
-long	memcached;	/* Cached */
-long	memshmem;	/* Shmem */
-long	memsreclaim;	/* SReclaimable */
-long	memused;	/* memtotal - memfree - membuffers - memcached - memsreclaim + memshmem */
-double	mem_usage;	/* memused / memtotal * 100 */
-double	cpu_usage;	/* (working time) / (working time + idle time) * 100 */
-double	diskio_usage;	/* disk IO utilization */
-double	load[3];	/* CPU load */
+unsigned long	memtotal;	/* MemTotal */
+unsigned long	memfree;	/* MemFree */
+unsigned long	membuffers;	/* Buffers */
+unsigned long	memcached;	/* Cached */
+unsigned long	memshmem;	/* Shmem */
+unsigned long	memsreclaim;	/* SReclaimable */
+unsigned long	memused;	/* memtotal - memfree - membuffers - memcached - memsreclaim + memshmem */
+double		mem_usage;	/* memused / memtotal * 100 */
+double		cpu_usage;	/* (working time) / (working time + idle time) * 100 */
+struct disks_t	disks = { NULL, NULL };
 
 int main (void)
 {
@@ -37,47 +38,38 @@ int main (void)
 	keypad (stdscr, TRUE);	/* enable keypad */
 	noecho ();		/* no echo */
 	timeout (INTERVAL);	/* input blocking for INTERVAL milliseconds */
-	pre_draw ();
 
 	while (1) {
-		draw ();
+		draw_cpu ();
+		draw_mem ();
+		draw_diskio ();
 		refresh ();
+
 		c = getch ();
-		if (c == 'q' || c == '') {
+		if (c == 'q' || c == '')
 			break;
-		} else if (c == KEY_RESIZE) {	/* handling SIGWINCH signal would be more compatible */
+		else if (c == KEY_RESIZE)	/* handling SIGWINCH signal would be more compatible */
 			clear ();
-			pre_draw ();
-		}
 	}
 
 	/* end curses mode */
-	endwin();
+	endwin ();
+	free_disks ();
 
 	return 0;
 }
 
-void pre_draw (void)
+void draw_cpu (void)
 {
-	/* show CPU usage before the blocking calculation */
+	static pthread_t	thread;
+
+	if (pthread_create (&thread, NULL, (void *(*)(void *)) &read_cpu, NULL) != 0)
+		error ("failed to create thread for calculating cpu usage");
+
 	mvprintw (1, 2, "CPU: %5.1lf%%", cpu_usage);
 }
 
-void draw (void)
-{
-	pthread_t	cpu_thread;
-
-	if (pthread_create (&cpu_thread, NULL, (void *(*)(void *)) &draw_cpu, NULL) != 0)
-		error ("failed to create thread for calculating cpu usage");
-	draw_mem ();
-	draw_diskio ();
-}
-
-void draw_diskio (void)
-{
-}
-
-void draw_cpu (void)
+void read_cpu (void)
 {
 	double	s[4], t[4];
 	FILE	*fin;
@@ -94,40 +86,44 @@ void draw_cpu (void)
 	cpu_usage = ((t[0] + t[1] + t[2]) - (s[0] + s[1] + s[2])) /
 		((t[0] + t[1] + t[2] + t[3]) - (s[0] + s[1] + s[2] + s[3])) *
 		100.0;
-
-	mvprintw (1, 2, "CPU: %5.1lf%%", cpu_usage);
 }
 
 void draw_mem (void)
+{
+	read_mem ();
+	mvprintw (3, 2, "Memory: %5.1lf%% (%ld kB / %ld kB)", mem_usage, memused, memtotal);
+}
+
+void read_mem (void)
 {
 	FILE	*fin;
 
 	if ((fin = fopen ("/proc/meminfo", "r")) == NULL)
 		error ("failed to open /proc/meminfo");
 	fscanf (fin,
-		"%*[^0-9]%ld"	/* MemTotal       */
-		"%*[^0-9]%ld"	/* MemFree        */
-		"%*[^0-9]%*ld"	/* MemAvailable   */
-		"%*[^0-9]%ld"	/* Buffers        */
-		"%*[^0-9]%ld"	/* Cached         */
-		"%*[^0-9]%*ld"	/* SwapCached     */
-		"%*[^0-9]%*ld"	/* Active         */
-		"%*[^0-9]%*ld"	/* Inactive       */
-		"%*[^0-9]%*ld"	/* Active(anon)   */
-		"%*[^0-9]%*ld"	/* Inactive(anon) */
-		"%*[^0-9]%*ld"	/* Active(file)   */
-		"%*[^0-9]%*ld"	/* Inactive(file) */
-		"%*[^0-9]%*ld"	/* Unevictable    */
-		"%*[^0-9]%*ld"	/* Mlocked        */
-		"%*[^0-9]%*ld"	/* SwapTotal      */
-		"%*[^0-9]%*ld"	/* SwapFree       */
-		"%*[^0-9]%*ld"	/* Dirty          */
-		"%*[^0-9]%*ld"	/* Writeback      */
-		"%*[^0-9]%*ld"	/* AnonPages      */
-		"%*[^0-9]%*ld"	/* Mapped         */
-		"%*[^0-9]%ld"	/* Shmem          */
-		"%*[^0-9]%*ld"	/* Slab           */
-		"%*[^0-9]%ld"	/* SReclaimable   */
+		"%*[^0-9]%lu"	/* MemTotal       */
+		"%*[^0-9]%lu"	/* MemFree        */
+		"%*[^0-9]%*lu"	/* MemAvailable   */
+		"%*[^0-9]%lu"	/* Buffers        */
+		"%*[^0-9]%lu"	/* Cached         */
+		"%*[^0-9]%*lu"	/* SwapCached     */
+		"%*[^0-9]%*lu"	/* Active         */
+		"%*[^0-9]%*lu"	/* Inactive       */
+		"%*[^0-9]%*lu"	/* Active(anon)   */
+		"%*[^0-9]%*lu"	/* Inactive(anon) */
+		"%*[^0-9]%*lu"	/* Active(file)   */
+		"%*[^0-9]%*lu"	/* Inactive(file) */
+		"%*[^0-9]%*lu"	/* Unevictable    */
+		"%*[^0-9]%*lu"	/* Mlocked        */
+		"%*[^0-9]%*lu"	/* SwapTotal      */
+		"%*[^0-9]%*lu"	/* SwapFree       */
+		"%*[^0-9]%*lu"	/* Dirty          */
+		"%*[^0-9]%*lu"	/* Writeback      */
+		"%*[^0-9]%*lu"	/* AnonPages      */
+		"%*[^0-9]%*lu"	/* Mapped         */
+		"%*[^0-9]%lu"	/* Shmem          */
+		"%*[^0-9]%*lu"	/* Slab           */
+		"%*[^0-9]%lu"	/* SReclaimable   */
 		,
 		&memtotal,
 		&memfree,
@@ -138,13 +134,111 @@ void draw_mem (void)
 	fclose (fin);
 	memused = memtotal - memfree - membuffers - memcached - memsreclaim + memshmem;
 	mem_usage = (double)memused / (double)memtotal * 100.0;
+}
 
-	mvprintw (3, 2, "Memory: %5.1lf%% (%ld kB / %ld kB)", mem_usage, memused, memtotal);
+void draw_diskio (void)
+{
+	static pthread_t	thread;
+	struct disk_t		*d;
+	int			i;
+
+	if (pthread_create (&thread, NULL, (void *(*)(void *)) &read_diskio, NULL) != 0)
+		error ("failed to create thread for calculating diskio usage");
+
+	mvaddstr (5, 2, "Disk I/O:");
+	for (d = disks.head, i = 0; d != NULL; d = d->next, i += 4) {
+		mvprintw (7 + i, 6, "- %s", d->name);
+		mvprintw (8 + i, 10,  "read:  %.1lf kB/sec", d->rkbps);
+		mvprintw (9 + i, 10,  "write: %.1lf kB/sec", d->wkbps);
+		mvprintw (10 + i, 10, "total: %.1lf kB/sec", d->totalkbps);
+	}
+}
+
+void read_diskio (void)
+{
+	struct disk_t *d;
+
+	read_diskstats (0);
+	usleep (DISKIO_INT);
+	read_diskstats (1);
+
+	for (d = disks.head; d != NULL; d = d->next) {
+		d->rkbps = (double)(d->rsect[1] - d->rsect[0]) / 2.0
+			* 1000000.0 / (double)(DISKIO_INT);
+		d->wkbps = (double)(d->wsect[1] - d->wsect[0]) / 2.0
+			* 1000000.0 / (double)(DISKIO_INT);
+		d->totalkbps = (double)(d->rsect[1] - d->rsect[0] + d->wsect[1] - d->wsect[0]) / 2.0
+			* 1000000.0 / (double)(DISKIO_INT);
+	}
+}
+
+void read_diskstats (int sample)
+{
+	FILE		*fin;
+	char		entry[DISKSTATS_BUF];
+	struct disk_t	disk;
+
+	if ((fin = fopen ("/proc/diskstats", "r")) == NULL)
+		error ("failed to open /proc/diskstats");
+	while (fgets (entry, DISKSTATS_BUF, fin) != NULL) {
+		sscanf (entry, "%d %d %s %*lu %*lu %lu %*lu %*lu %*lu %lu",
+			&(disk.major),
+			&(disk.minor),
+			disk.name,
+			&(disk.rsect[sample]),
+			&(disk.wsect[sample]));
+		if (disk.major != 8 || (disk.minor % 16 != 0))
+			continue;
+		if (update_disk (&disk, sample) < 0 && sample == 0)
+			append_disk (&disk);
+	}
+	fclose (fin);
+}
+
+int update_disk (const struct disk_t *disk, int sample)
+{
+	struct disk_t *d;
+	for (d = disks.head; d != NULL; d = d->next) {
+		if (d->major != disk->major || d->minor != disk->minor)
+			continue;
+		d->rsect[sample] = disk->rsect[sample];
+		d->wsect[sample] = disk->wsect[sample];
+		return 0;
+	}
+	return -1;
+}
+
+void append_disk (const struct disk_t *disk)
+{
+	struct disk_t	*new_disk;
+
+	if ((new_disk = malloc (sizeof (struct disk_t))) == NULL)
+		error ("failed to allocate memory for new disk structure");
+	(*new_disk) = (*disk);
+
+	new_disk->previous = disks.tail;
+	if (disks.tail)
+		disks.tail->next = new_disk;
+	else
+		disks.head = new_disk;
+	new_disk->next = NULL;
+	disks.tail = new_disk;
+}
+
+void free_disks (void)
+{
+	struct disk_t *d = disks.head, *n;
+	while (d != NULL) {
+		n = d->next;
+		free (d);
+		d = n;
+	}
 }
 
 void error (const char *s)
 {
 	endwin();
+	free_disks ();
 	if (s)
 		fprintf (stderr, "[Error] %s\n", s);
 	exit (!!s);
--- monitor.h
+++ monitor.h
@@ -1,11 +1,33 @@
 #ifndef __MONITOR_H
 #define __MONITOR_H
 
-void	pre_draw (void);	/* preparations before drawing */
-void	draw (void);		/* construct the screen output */
-void	draw_cpu (void);	/* draw the CPU utilization */
-void	draw_mem (void);	/* draw the memory utilization */
-void	draw_diskio (void);	/* draw the disk IO utilization */
+#define DISKNAME_SIZE 32
+
+struct disk_t {
+	int		major, minor;
+	char		name[DISKNAME_SIZE];
+	unsigned long	rsect[2], wsect[2];
+	double		rkbps, wkbps, totalkbps;
+	struct disk_t	*previous, *next;
+};
+
+struct disks_t {
+	struct disk_t	*head, *tail;
+};
+
 void	error (const char *);	/* report error and quit */
+
+void	draw_cpu (void);	/* draw the CPU utilization */
+void	read_cpu (void);	/* blocking; running with pthread */
+
+void	draw_mem (void);	/* draw the memory utilization */
+void	read_mem (void);
+
+void	draw_diskio (void);	/* draw the disk IO utilization */
+void	read_diskio (void);	/* blocking; running with pthread */
+void	read_diskstats (int);
+int	update_disk (const struct disk_t *, int);
+void	append_disk (const struct disk_t *);
+void	free_disks (void);
 
 #endif
